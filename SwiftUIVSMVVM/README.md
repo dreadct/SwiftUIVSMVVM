8f67080bd5c592825b1860a9ed7cd413954cd2b1
Vadim Zhilinkov <dreadct@gmail.com>
2020-05-24 11:52:39 +0700

    Initial Commit

---
+++ SwiftUIVSMVVM/README.md
@@ -0,0 +1,6 @@
+# SwiftUIVSMVVM
+
+> A test SwiftUI application implementing a ViewState MVVM architecture.
+
+[![Swift](https://img.shields.io/badge/Swift-5.1-orange.svg)](https://swift.org)
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
---
+++ SwiftUIVSMVVM/UI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIVSMVVM
+//
+//  Created by Vadim Zhilinkov on 24.05.2020.
+//  Copyright © 2020 dreadct. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
